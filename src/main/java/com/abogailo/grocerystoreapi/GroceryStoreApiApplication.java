3220b2e12962d8c4bbc447f5805fb796616582a9
abogailo <abagailanne@gmail.com>
2020-05-21 18:09:13 -0500

    first commit

---
+++ src/main/java/com/abogailo/grocerystoreapi/GroceryStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.abogailo.grocerystoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryStoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryStoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abogailo/grocerystoreapi/GroceryStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abogailo.grocerystoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
